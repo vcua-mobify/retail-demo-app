1094874fccd985e6600dcb96e878c215fcf5c63e
vcua-mobify <vcua@salesforce.com>
2022-01-05 16:32:26 -0800

    Initial commit

---
+++ app/commerce-api.config.js
@@ -0,0 +1,9 @@
+export const commerceAPIConfig = {
+    proxyPath: `/mobify/proxy/api`,
+    parameters: {
+        clientId: '1d763261-6522-4913-9d52-5d947d3b94c4',
+        organizationId: 'f_ecom_zzte_053',
+        shortCode: 'kv7kzm78',
+        siteId: 'RefArch'
+    }
+}
